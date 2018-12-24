b7a93edfef676e6df8ffa013ea229140e04b3cd7
Mateusz Dudek <mateusz.dudek@predica.pl>
2018-12-24 13:19:10 +0100

    Modify Subnet deployment for VNET

--- Week3/2-i-4/run.ps1
+++ Week3/2-i-4/run.ps1
@@ -1,4 +1,4 @@
 New-AzureRmResourceGroupDeployment -Name "Week3" `
     -ResourceGroupName uinfrne1rgp `
-    -TemplateFile .\2-i-4\template.json `
-    -TemplateParameterFile .\2-i-4\template.parameters.json
+    -TemplateFile .\template.json `
+    -TemplateParameterFile .\template.parameters.json
